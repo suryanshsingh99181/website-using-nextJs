e673ba2588576b1bf300695e0aace52d5fd0189c
suryanshsingh99181 <suryanshsingh99181@gmail.com>
2024-11-13 15:50:52 +0530

    removed unused variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import FeaturedCourses from "@/components/FeaturedCourses";
 import HearOurHarmony from "@/components/HearOurHarmony";
 import HeroSection from "@/components/HeroSection";
 import WhyChooseUs from "@/components/WhyChooseUs";
-import Image from "next/image";
 
 export default function Home() {
   return (
